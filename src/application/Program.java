8c3b76961d74e07d437b31a89a423abe949f50a4
Yuri Baldan <yuribbp@gmail.com>
2020-10-01 16:32:47 -0300

    UPDATE Inplementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import db.DB;
 
@@ -8,8 +10,30 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		
 		Connection conn = null;
-		DB.closeConnection();
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+	
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 }
